77fa8421193eb690947d23e91e55062805ce5aef
Glitchkittenrocks <carknerian3@gmail.com>
2026-01-10 14:39:09 -0500

    Added template enchantment code

--- src/main/resources/assets/goodblock/Enchantments/ThreeMonthArrowCurse.java
+++ src/main/resources/assets/goodblock/Enchantments/ThreeMonthArrowCurse.java
@@ -1,1 +1,21 @@
+package net.hhdsjgit.goodblock.enchantment;
+
+import net.minecraft.world.item.enchant.Enchantment;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.enchant.EnchantmentCatagory;
+
+public class ThreeMonthArrow extends Enchantment {
+       
+  public ThreeMonthArrow() {
+    super(Enchantment.Rarity.Common, EnchantmentCatergory.Bow, new EquipmentSlot[] {EquipmentSlot.MAINHAND});
+
+  }
+
+   @Override
+   public int getMaxLevel() {
+    return 1;
+   }
+
+  
+}
 
